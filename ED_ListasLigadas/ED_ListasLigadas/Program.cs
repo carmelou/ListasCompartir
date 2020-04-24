e0472986ad668f0a002e5f06c2f1a2ea9f5c5eee
carmelou <63168650+carmelou@users.noreply.github.com>
2020-04-23 20:40:12 -0600

    Add files via upload

---
+++ ED_ListasLigadas/ED_ListasLigadas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED_ListasLigadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista lista=new Lista();
+            if(lista.EstaVacia())
+                {
+            Console.WriteLine("esta vacia");
+            }
+            else {
+            Console.WriteLine("no esta vacia");}
+            lista.AgregarNodo("uno");
+lista.AgregarNodo("dos");
+lista.AgregarNodo("tres");
+            Console.ReadKey();
+
+
+        }
+    }
+}
